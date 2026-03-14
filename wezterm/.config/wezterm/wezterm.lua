3500a9ee72d56085b0f46298c5f877bdc587d28e
Pheera <pheeraphuangphi@gmail.com>
2026-03-15 04:48:21 +0700

    set font size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
-config.font_size = 12
+config.font_size = 11
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
 
 config.treat_east_asian_ambiguous_width_as_wide = false
