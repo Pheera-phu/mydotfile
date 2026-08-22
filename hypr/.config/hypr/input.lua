3a1314c95a43f794a2b5fdaf14b39262f5143df9
Pheera <pheeraphuangphi@gmail.com>
2026-08-23 01:23:58 +0700

    hyprland to lua overhaul

---
+++ hypr/.config/hypr/input.lua
@@ -0,0 +1,12 @@
+-- See https://wiki.hypr.land/Configuring/Basics/Variables/#input
+hl.config({
+  input = {
+    kb_layout = "us, th",
+    kb_options = "grp:caps_toggle",
+    repeat_rate = 40,
+    repeat_delay = 400,
+    numlock_by_default = true,
+    sensitivity = 0.0,
+    accel_profile = "flat",
+  },
+})
